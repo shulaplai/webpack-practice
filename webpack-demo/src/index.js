77975d5221c186056a185fc4ee92ed80f1b7aea8
Your Name <you@example.com>
2021-08-09 18:44:48 +0800

    push

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,36 +1,18 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
-import Data from "./data.xml";
-import Notes from "./data.csv";
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
+import printMe from "./print.js";
 
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // lodash 在当前 script 中使用 import 引入
+  
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-  // 将图像添加到我们已经存在的 div 中。
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-  console.log(Data);
-  console.log(Notes);
+  element.appendChild(btn);
   return element;
 }
 
