02d9fc1571394ebe9356d3ca33533520b8e458c9
Your Name <you@example.com>
2021-08-09 18:29:35 +0800

    push

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,6 +1,9 @@
 import _ from "lodash";
 import "./style.css";
 import Icon from "./icon.png";
+import Data from "./data.xml";
+import Notes from "./data.csv";
+
 
 function component() {
   const element = document.createElement("div");
@@ -15,7 +18,8 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
-
+  console.log(Data);
+  console.log(Notes);
   return element;
 }
 
