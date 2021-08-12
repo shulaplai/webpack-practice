1006fdde97ab09bf9c4cf762f7ef5d05eab9a16c
Your Name <you@example.com>
2021-08-12 15:06:24 +0800

    Finish for now

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,21 +1,17 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 
 module.exports = {
   entry: {
     index: "./src/index.js",
-    print: "./src/print.js",
+    another: "./src/another-module.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "管理输出",
-    }),
-  ],
+
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: "/",
   },
 };
 
